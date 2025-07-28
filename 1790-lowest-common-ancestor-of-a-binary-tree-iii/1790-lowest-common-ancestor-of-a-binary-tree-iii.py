35481f3d0d3808ab0f2cb8802924b666b40a701a
Nikhath Firdose <157666020+nikhathfirdose1@users.noreply.github.com>
2025-07-27 21:53:07 -0700

    Time: 66 ms (5.16%), Space: 20.8 MB (82.48%) - LeetHub

---
+++ 1790-lowest-common-ancestor-of-a-binary-tree-iii/1790-lowest-common-ancestor-of-a-binary-tree-iii.py
@@ -0,0 +1,29 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        self.parent = None
+"""
+
+class Solution:
+    def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
+
+        a = p
+        b = q
+
+        while a != b:
+
+            if a.parent: 
+                a = a.parent
+            else:
+                a = q
+
+            if b.parent:
+                b = b.parent
+            else:
+                b = p
+
+        return a
