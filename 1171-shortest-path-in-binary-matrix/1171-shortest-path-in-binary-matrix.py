6bc1f6e0d500a0c5389fe6e2cf3ded4e1b39aa0c
Nikhath Firdose <157666020+nikhathfirdose1@users.noreply.github.com>
2025-08-16 12:19:56 -0700

    Time: 125 ms (89.51%), Space: 18.4 MB (71.97%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
@@ -3,22 +3,15 @@ class Solution:
 
         n = len(grid)
 
-        if n == 1:
-            if grid[0][0] == 0:
-                return 1
-
         if grid[0][0] == 1 or grid[n-1][n-1] == 1:
             return -1
 
         
         queue = deque([(0,0,1)])
-        visited = set()
 
         while queue:
 
             row, col, dist  = queue.popleft()
-
-            visited.add((row,col))
 
             if row == n-1 and col == n-1:
                 return dist
@@ -26,13 +19,13 @@ class Solution:
             directions = [(1,0), (0,1), (-1,0), (0,-1), (-1,-1), (1,1), (1,-1), (-1,1)]
 
             for dr, dc in directions:
-                nr, nc = row + dr, col +dc
+                nr, nc = row + dr, col + dc
 
-                if 0 <= nr < n and 0 <= nc < n and (nr,nc) not in visited:
+                if 0 <= nr < n and 0 <= nc < n and grid[nr][nc] != 1:
 
                     if grid[nr][nc] == 0:
+                        grid[nr][nc] = 1
                         queue.append((nr,nc, dist+1))
-                        visited.add((nr,nc))
                     
 
         return -1
