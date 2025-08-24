b2cde9ac3be1a954e19b910a4fde5ea49b0b047c
Nikhath Firdose <157666020+nikhathfirdose1@users.noreply.github.com>
2025-08-24 10:16:13 -0700

    Time: 216 ms (41.56%), Space: 19.5 MB (41.85%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
@@ -1,35 +1,44 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
 
-        n = len(grid)
 
-        if grid[0][0] == 1 or grid[n-1][n-1] == 1:
+
+        rows = len(grid)
+        cols = len(grid[0])
+
+        if grid[0][0] !=0 or grid[rows-1][cols-1] !=0:
             return -1
 
-        queue = deque([(0,0,1)])
-        grid[0][0] = 1
 
-        directions = [(1,0), (0,1), (-1,0), (0,-1), (-1,-1), (1,1), (1,-1), (-1,1)]
+        queue = deque([(0,0,1)])
+        visited = set()
+        visited.add((0,0))
+
+        directions = [(1,0), (0,1), (-1,0), (0,-1), (-1,-1), (-1,1), (1,-1),(1,1)]
 
         while queue:
+            row, col, dist = queue.popleft()
 
-            row, col, dist  = queue.popleft()
-
-            if row == n-1 and col == n-1:
+            if (row, col) == (rows-1, cols -1):
                 return dist
 
+            for dr,dc in directions:
+                nr, nc = row + dr, col + dc 
 
-            for dr, dc in directions:
-                nr, nc = row + dr, col + dc
 
-                if 0 <= nr < n and 0 <= nc < n and grid[nr][nc] != 1:
-
-                    if grid[nr][nc] == 0:
-                        grid[nr][nc] = 1
-                        queue.append((nr,nc, dist+1))
+                if 0 <= nr < rows and 0 <= nc < cols and (nr,nc) not in visited:
                     
 
+                    if grid[nr][nc] == 0:
+                        # prin÷t(nr,nc)
+                        
+                        queue.append((nr,nc, dist+1))
+                        visited.add((nr,nc))
+
+
         return -1
+
+                    
 
 
 
