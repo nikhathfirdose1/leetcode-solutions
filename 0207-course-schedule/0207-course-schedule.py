2de872f6a5c7935ab2db1b5a2f12e041bb38efd4
Nikhath Firdose <157666020+nikhathfirdose1@users.noreply.github.com>
2025-08-26 11:04:16 -0700

    Time: 4 ms (67.98%), Space: 19.2 MB (51.53%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,32 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+
+        indegree =  [0] * numCourses
+        
+        adj = defaultdict(list)
+
+        for u,v in prerequisites:
+            adj[u].append(v)
+            indegree[v] += 1
+
+        queue = deque()
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                queue.append(i)
+
+        
+        topo = []
+
+        while queue:
+            node = queue.popleft()
+            topo.append(node)
+
+
+            for neigh in adj[node]:
+                indegree[neigh] -= 1
+
+                if indegree[neigh] == 0:
+                    queue.append(neigh)
+
+        
+        return len(topo) == numCourses
