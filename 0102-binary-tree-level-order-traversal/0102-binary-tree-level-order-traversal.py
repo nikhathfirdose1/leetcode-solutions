a1b36a7702afa7b41179fee9520a0334925a0dfa
Nikhath Firdose <157666020+nikhathfirdose1@users.noreply.github.com>
2025-08-31 12:06:37 -0700

    Time: 0 ms (100%), Space: 18.5 MB (59.32%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -10,27 +10,26 @@ class Solution:
         if not root:
             return []
 
-        queue = deque([root])
         ans = []
 
-        while queue:
+        def dfs(node, level):
+            nonlocal ans
 
-            size = len(queue)
-            level = []
+            if len(ans) == level:
+                ans.append([])
 
-            for i in range(size):
-                node = queue.popleft()
-                level.append(node.val)
+            ans[level].append(node.val)
 
-                if node.left:
-                    queue.append(node.left)
-                
-                if node.right:
-                    queue.append(node.right)
+            if node.left:
+                dfs(node.left, level+1)
+            if node.right:
+                dfs(node.right, level + 1)
 
-            ans.append(level)
-
+        
+        dfs(root,0)
         return ans
+
+            
 
 
         
