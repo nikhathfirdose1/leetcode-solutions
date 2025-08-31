180810073c101fba4c3453cae3f2845494c6d4f3
Nikhath Firdose <157666020+nikhathfirdose1@users.noreply.github.com>
2025-08-30 17:38:44 -0700

    Time: 125 ms (89.09%), Space: 18.1 MB (92%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
@@ -1,33 +1,32 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
 
-        rows = len(grid)
-        cols = len(grid[0])
+        m = len(grid)
+        n = len(grid[0])
+        
 
-        if grid[0][0] != 0 or grid[rows-1][cols-1] != 0:
-            return -1
+        if grid[0][0] != 0 or grid[m-1][n-1] != 0:
+            return -1 
 
         queue = deque([(0,0,1)])
+        grid[0][0] = 1
 
-        directions = [(1,0) ,(0,1), (-1,0) , (0,-1), (1,1), (-1,-1), (1,-1), (-1,1)]
+        directions = [(1,0) , (0,1) , (-1,0) , (0,-1), (1,1), (1,-1) , (-1,1), (-1,-1)]
 
         while queue:
             row, col, dist = queue.popleft()
 
-            if (row,col) == (rows-1, cols-1):
+            if row == m-1 and col == n-1:
                 return dist
 
-            
             for dr, dc in directions:
                 nr, nc = row + dr, col + dc
 
-                if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] != 1:
-                    if grid[nr][nc] == 0:
-                        grid[nr][nc] = 1
-                        queue.append((nr,nc,dist+1))
-
+                if 0 <= nr < m and 0 <= nc <n and grid[nr][nc] == 0:
+                    grid[nr][nc] = 1
+                    queue.append((nr,nc,dist+1))
+                        
+                        
 
         return -1
 
-        
-        
