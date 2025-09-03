3bbfa95bac66e2e70ee8352a0c384dfe70d9ae16
Nikhath Firdose <157666020+nikhathfirdose1@users.noreply.github.com>
2025-09-02 18:28:53 -0700

    Time: 74 ms (76.87%), Space: 20.2 MB (74.01%) - LeetHub

--- 0348-design-tic-tac-toe/0348-design-tic-tac-toe.py
+++ 0348-design-tic-tac-toe/0348-design-tic-tac-toe.py
@@ -1,48 +1,33 @@
 class TicTacToe:
 
     def __init__(self, n: int):
+
         self.n = n
-        self.board = [[0] * n for _ in range(n)]
+        self.rows = [0] * self.n
+        self.cols = [0] * self.n
+        self.diag = 0
+        self.anti = 0
+        
 
     def move(self, row: int, col: int, player: int) -> int:
-        self.board[row][col] = player
 
-        if self.checkRow(row, player) or self.checkCol(col, player) or (row == col and self.checkDiag(player)) or (col == self.n - row - 1 and self.checkAntiDiag(player)):
+        add = 1 if player == 1 else - 1
+
+
+        self.rows[row] += add
+        self.cols[col] += add
+
+        if row == col:
+            self.diag += add
+        if row + col == self.n - 1:
+            self.anti += add
+
+
+        if abs(self.rows[row]) == self.n or abs(self.cols[col]) == self.n or abs(self.diag) == self.n or abs(self.anti) == self.n:
             return player
 
         return 0
 
-    
-    def checkRow(self, row, player):
-        for col in range(self.n):
-            if self.board[row][col] != player:
-                return False
-        
-        return True
-
-    def checkCol(self, col, player):
-        for row in range(self.n):
-            if self.board[row][col] != player:
-                return False
-
-        return True
-
-    
-    def checkDiag(self, player):
-        for row in range(self.n):
-            if self.board[row][row] != player:
-                return False
-
-        return True
-
-    def checkAntiDiag(self, player):
-        for row in range(self.n):
-            if self.board[row][self.n-row-1] != player:
-                return False
-        
-        return True
-
-        
 
 # Your TicTacToe object will be instantiated and called as such:
 # obj = TicTacToe(n)
