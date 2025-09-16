9526ecc6d5b8c9aa5ed47764a6c819a8e68e831f
Nikhath Firdose <157666020+nikhathfirdose1@users.noreply.github.com>
2025-09-15 21:56:54 -0700

    Time: 0 ms (100%), Space: 18.3 MB (22.52%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,21 +1,6 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
 
-        def bs(s,e):
-
-            while s <=e:    
-                m = (s+e) // 2
-
-                if nums[m] == target:
-                    return m
-
-                elif target < nums[m]:
-                    e = m -1
-                
-                else:
-                    s = m + 1
-
-            return -1 
 
         n = len(nums)
 
@@ -24,30 +9,26 @@ class Solution:
 
         while left <= right:
 
-            if nums[left] <= nums[right]:            
-                return bs(left, right)
 
             mid = (left + right) // 2
-            prev = (mid + n -1) % n
-            nxt = (mid +1)% n
 
-            if nums[prev] >= nums[mid] and nums[nxt] >= nums[mid]: 
-                if nums[mid] <= target <= nums[right]:
-                    return bs(mid, right)
-                else:
-                    return bs(0, mid - 1)
+            if nums[mid] == target:
+                return mid
 
-            if nums[left] <= nums[mid]:
+
+            elif nums[left] <= nums[mid]:
             
                 if nums[left] <= target <= nums[mid]:
-                    return bs(left, mid)
-                
-                left = mid + 1
+                    right = mid -1 
+                else:
+
+                    left = mid + 1
             else:
                 
                 if nums[mid] <= target <= nums[right]:
-                    return bs(mid, right)
-                right = mid - 1
+                    left = mid +1 
+                else:
+                    right = mid - 1
 
 
         return -1
