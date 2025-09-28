cbeb1575e11ac8db863485559650f933dfdcd836
Nikhath Firdose <nikhath.firdose@sjsu.edu>
2025-09-27 21:30:14 -0700

    Time: 3 ms (89.42%), Space: 18.3 MB (38.99%) - LeetHub

--- 0415-add-strings/0415-add-strings.py
+++ 0415-add-strings/0415-add-strings.py
@@ -7,27 +7,30 @@ class Solution:
         p1 = len(num1) - 1
         p2 = len(num2) - 1
 
-        while p1 >= 0 or p2 >= 0:
-            if p1>= 0:
-                x1 = ord(num1[p1]) - ord('0')
-            else:
-                x1 = 0
-
-            if p2 >= 0:
-                x2 = ord(num2[p2]) - ord('0')
-            else:
-                x2 = 0
+        while p1 >= 0 or p2>=0:
             
-            value = (x1+ x2 + carry) % 10
-            carry = (x1+ x2 + carry) // 10
+            if p1 < 0:
+                x1 = 0
+            else:
+                x1 = ord(num1[p1]) - ord("0")
+
+            
+            
+            if p2 < 0:
+                x2 = 0
+            else:
+                x2 = ord(num2[p2]) - ord("0")
+
+            value = (x1 + x2 + carry) % 10
+            carry = (x1 + x2 + carry) // 10
 
             res.append(value)
 
             p1 -= 1
             p2 -= 1
 
-        if carry:
+        if carry != 0:
             res.append(carry)
 
+        
         return "".join(str(x) for x in res[::-1])
-
