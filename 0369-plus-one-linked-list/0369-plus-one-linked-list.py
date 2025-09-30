967846bef4f8ca458978f5fd57ffe9cd206fd0bf
Nikhath Firdose <nikhath.firdose@sjsu.edu>
2025-09-29 17:15:56 -0700

    Time: 46 ms (8.48%), Space: 17.9 MB (11.4%) - LeetHub

---
+++ 0369-plus-one-linked-list/0369-plus-one-linked-list.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def plusOne(self, head: ListNode) -> ListNode:
+        
+        dummy = ListNode(0)
+        dummy.next = head
+        notNine = dummy
+
+        while head:
+
+            if head.val != 9:
+                notNine = head
+            
+            head = head.next
+
+        
+        notNine.val += 1
+        notNine = notNine.next
+
+        while notNine:
+            notNine.val = 0
+            notNine = notNine.next
+
+
+        return dummy if dummy.val else dummy.next
+
+
