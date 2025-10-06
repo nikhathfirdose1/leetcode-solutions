67005de7bb8690b1516c92a0d2b6706e04a4472e
Nikhath Firdose <nikhath.firdose@sjsu.edu>
2025-10-05 18:36:34 -0700

    Time: 19 ms (8.34%), Space: 18.2 MB (44.92%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,29 +1,47 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+
+
+
+        def check_alpha(st):
+
+            if ord("a") <= ord(st) <= ord("z"):
+
+                return True
+
+            elif ord("A") <= ord(st) <= ord("Z"):
+
+                return True
+
+            elif ord("0") <= ord(st) <= ord("9"):
+                return True
+
+            else:
+                return False
+
+            return False
         
         left = 0
         right = len(s) - 1
 
         while left <= right:
 
-            while left < right and not s[left].isalnum():
+            while left < right and not check_alpha(s[left]):
                 left += 1
 
-            while left < right and not s[right].isalnum():
+            while left < right and not check_alpha(s[right]):
                 right -= 1
 
-            # print(left, right)
 
-            if s[left].lower() != s[right].lower():
-                
+            if s[left].lower() != s[right].lower():   
                 return False
 
             
             left += 1
             right -= 1
 
-            
-
-
 
         return True
+
+
+
