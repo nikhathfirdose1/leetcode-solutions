eee89b5f5a650ec19ddc2afff53dbcf62c0a6888
Nikhath Firdose <nikhath.firdose@sjsu.edu>
2025-11-16 22:41:52 -0800

    Time: 43 ms (39.24%), Space: 18.4 MB (46.02%) - LeetHub

---
+++ 0766-flatten-a-multilevel-doubly-linked-list/0766-flatten-a-multilevel-doubly-linked-list.py
@@ -0,0 +1,48 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, prev, next, child):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
+"""
+
+class Solution:
+    def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
+
+        if not head:
+            return head
+        
+
+        dummy = Node(0)
+        curr = dummy
+
+        stack = [head]
+
+        while stack:
+
+            temp = stack.pop()
+
+            if temp.next:
+                stack.append(temp.next)
+
+            
+            if temp.child:
+                stack.append(temp.child)
+
+            
+            curr.next = temp
+
+            temp.prev = curr
+
+            temp.child = None
+
+
+            curr = curr.next
+
+        
+        dummy.next.prev=None
+
+
+        return dummy.next
