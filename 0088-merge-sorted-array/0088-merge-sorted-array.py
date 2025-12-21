9b8dbbfed9f3fa7f37a754f06d6aa0ab4010c3c0
Nikhath Firdose <nikhath.firdose@sjsu.edu>
2025-12-21 12:55:42 -0800

    Time: 0 ms (100%), Space: 17.2 MB (99.17%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -2,33 +2,49 @@ class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
-
         """
+        
 
-        n1 = m - 1
-        n2 = n  - 1
-        res = (m + n) -1
+        f = len(nums1) - 1
+        n2 = len(nums2) - 1
+        n1 = f - n2 - 1
 
-        while n1 >= 0 and n2 >= 0:
+        
+        while n1 >=0 and n2 >=0:
+
             if nums1[n1] > nums2[n2]:
-                nums1[res] = nums1[n1]
+                nums1[f] = nums1[n1]
                 n1 -= 1
-            else:
-                nums1[res] = nums2[n2]
-                n2 -= 1
+                f -= 1
 
-            res -= 1
+            else:
+                nums1[f] = nums2[n2]
+                n2 -= 1
+                f -= 1
+
 
         while n2 >= 0:
-            nums1[res] = nums2[n2]
+            nums1[f] = nums2[n2]
             n2 -= 1
-            res -= 1
-
-
-
-        
-
+            f -= 1
         
 
 
-        
+        # n1 = m - 1
+        # n2 = n  - 1
+        # res = (m + n) -1
+
+        # while n1 >= 0 and n2 >= 0:
+        #     if nums1[n1] > nums2[n2]:
+        #         nums1[res] = nums1[n1]
+        #         n1 -= 1
+        #     else:
+        #         nums1[res] = nums2[n2]
+        #         n2 -= 1
+
+        #     res -= 1
+
+        # while n2 >= 0:
+        #     nums1[res] = nums2[n2]
+        #     n2 -= 1
+        #     res -= 1
