ca99009770cb198302ba05cd3342f6f2d12e0bc6
Nikhath Firdose <nikhath.firdose@sjsu.edu>
2025-12-21 12:55:51 -0800

    Time: 0 ms (100%), Space: 17.2 MB (99.17%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -28,23 +28,3 @@ class Solution:
             n2 -= 1
             f -= 1
         
-
-
-        # n1 = m - 1
-        # n2 = n  - 1
-        # res = (m + n) -1
-
-        # while n1 >= 0 and n2 >= 0:
-        #     if nums1[n1] > nums2[n2]:
-        #         nums1[res] = nums1[n1]
-        #         n1 -= 1
-        #     else:
-        #         nums1[res] = nums2[n2]
-        #         n2 -= 1
-
-        #     res -= 1
-
-        # while n2 >= 0:
-        #     nums1[res] = nums2[n2]
-        #     n2 -= 1
-        #     res -= 1
