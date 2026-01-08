f23f4de84ffc45412a32d1861fe03c886e2cf978
Nikhath Firdose <nikhath.firdose@sjsu.edu>
2026-01-07 17:46:01 -0800

    Time: 62 ms (18.54%), Space: 19.9 MB (5.19%) - LeetHub

--- 1114-print-in-order/1114-print-in-order.py
+++ 1114-print-in-order/1114-print-in-order.py
@@ -1,25 +1,34 @@
-from threading import Event
+from threading import Semaphore
 
 class Foo:
     def __init__(self):
-        self.firstD = Event()
-        self.secondD = Event()
+
+        self.a = Semaphore(1)
+        self.b = Semaphore(0)
+        self.c = Semaphore(0)
+
+        pass
 
 
     def first(self, printFirst: 'Callable[[], None]') -> None:
         
+        # printFirst() outputs "first". Do not change or remove this line.
+        self.a.acquire()
         printFirst()
-        self.firstD.set()
+        self.b.release()
 
 
     def second(self, printSecond: 'Callable[[], None]') -> None:
         
-        if self.firstD.wait():
-            printSecond()
-            self.secondD.set()
+        # printSecond() outputs "second". Do not change or remove this line.
+        self.b.acquire()
+        printSecond()
+        self.c.release()
 
 
     def third(self, printThird: 'Callable[[], None]') -> None:
         
-        if self.secondD.wait():
-            printThird()
+        # printThird() outputs "third". Do not change or remove this line.
+        self.c.acquire()
+        printThird()
+        self.a.release()
