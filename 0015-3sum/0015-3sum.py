fff0a573d60188fc67398fa90a6e68915dfda840
Nikhath Firdose <nikhath.firdose@sjsu.edu>
2026-01-09 20:31:01 -0800

    Time: 711 ms (27.35%), Space: 24.4 MB (5.11%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -4,19 +4,34 @@ class Solution:
         ans = set()
         nums.sort()
 
+        out = 0
+        in_ = 0
+
+        
+
         for i in range(len(nums)):
+
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            
+            if nums[i] > 0:
+                break
             n2 = nums[i]
             seen = set()
+            out += 1
 
             for j in range(i+1, len(nums)):
                 n3 = nums[j]
                 n1 = -n2 -n3
+                in_ += 1
 
                 if n1 in seen:
                     triplet = tuple(((n1,n2,n3)))
                     ans.add(triplet)
 
                 seen.add(n3)
+
+        print(out,in_)
 
         return [list(t) for t in ans]
 
