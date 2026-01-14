ebdc9a1369a105af87eac72f9f9e62e19c009bca
Nikhath Firdose <nikhath.firdose@sjsu.edu>
2026-01-14 12:03:41 -0800

    Time: 0 ms (100%), Space: 19.4 MB (8.32%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,20 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+
+        if n <=2:
+            return n
+
+        dp = [0]* (n+1)
+
+        for i in range(3):
+            dp[i] = i
+        
+
+        for i in range(3, n+1):
+            dp[i] = dp[i-1]+ dp[i-2]
+
+        print(dp)
+        
+        return dp[n]
+        
