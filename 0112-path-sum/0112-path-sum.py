2a74eee14bbbfeb9de6692a6feba18d66dffdd27
Nikhath Firdose <nikhath.firdose@sjsu.edu>
2026-01-17 09:29:15 -0800

    Time: 0 ms (100%), Space: 20.2 MB (11.39%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+
+        if root is None:
+            return False
+
+        if not root.right and not root.left:
+            return targetSum == root.val
+
+        left = self.hasPathSum(root.left, targetSum - root.val)
+        right = self.hasPathSum(root.right, targetSum - root.val)
+
+        return left or right
+
+
+        
