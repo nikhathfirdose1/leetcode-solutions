177505c694683fef82ee0415aca8807b3493c60a
Nikhath Firdose <nikhath.firdose@sjsu.edu>
2026-02-22 11:29:44 -0800

    Time: 3840 ms (20.17%), Space: 81.2 MB (28.71%) - LeetHub

---
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.py
@@ -0,0 +1,40 @@
+class Solution:
+    def minKnightMoves(self, x: int, y: int) -> int:
+        
+        visited = set()
+
+        directions = [(1,2) , (1, -2), (-1, 2), (-1, -2), (2,1), (2,-1), (-2,-1), (-2, 1)]
+
+        dist = 0
+        queue = deque([(0,0, dist)])
+        visited.add((0,0))
+
+        a = abs(x)
+        b = abs(y)
+
+        while queue:
+
+            r, c, dist = queue.popleft()
+
+            min_r = - 2
+            min_c = - 2
+
+            max_r = a + 2
+            max_c = b + 2
+
+
+            if (r, c) == (a,b):
+                return dist
+
+            for dr, dc in directions:
+                nr = r + dr
+                nc = c + dc
+
+
+                if min_r > nr > max_r and min_c > nc > max_c:
+                    continue
+
+                if (nr, nc) not in visited:
+                    visited.add((nr,nc))
+                    queue.append((nr,nc, dist+1))
+        
