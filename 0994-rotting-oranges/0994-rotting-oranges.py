06d90227328dbfc9e733af6b54065bfaaa4dc3bf
Nikhath Firdose <nikhath.firdose@sjsu.edu>
2026-02-22 12:36:18 -0800

    Time: 4 ms (38.29%), Space: 19.5 MB (35.71%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -3,41 +3,52 @@ class Solution:
 
         rows = len(grid)
         cols = len(grid[0])
-        directions = [(-1,0), (0,-1), (1,0), (0,1)]
 
-        queue = deque([])
-        visited = set()
-        count = 0
 
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == 2:
-                    queue.append((r,c, count))
-                    visited.add((r,c))
+
+        directions = [(1,0), (0,1), (-1,0), (0,-1)]
+
+        queue = deque()
+        fresh = 0
+
+
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == 2:
+                    queue.append((row,col, 0))
+                elif grid[row][col] == 1:
+                    fresh += 1
+
+        if fresh == 0:
+            return 0
+        elif fresh > 0 and len(queue) == 0:
+            return -1
+        
 
         while queue:
 
-            r, c, count = queue.popleft()
+            r, c, minute = queue.popleft()
 
             for dr, dc in directions:
-                nr, nc = r+dr, c+dc
+                nr = r + dr
+                nc = c + dc           
 
-                if nr <0 or nr >= rows or nc<0 or nc >= cols or (nr,nc) in visited:
-                    continue
-
-                if grid[nr][nc] == 1:
-                    visited.add((nr,nc))
+                if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1:
                     grid[nr][nc] = 2
-                    queue.append((nr,nc, count+1))
-                    
+                    queue.append((nr,nc, minute + 1))
 
-        for r in range(rows):
-            # for c in range(cols):
-                if 1 in grid[r]:
+        
+
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == 1:
                     return -1
-
-        return count        
+                
+        return minute  
+        
 
 
             
 
+
+        
