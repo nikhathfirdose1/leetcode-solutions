2c5279eed43b3a87a52d4b9a9b60a6250f2e6b1c
Nikhath Firdose <nikhath.firdose@sjsu.edu>
2026-03-01 12:48:05 -0800

    Time: 3 ms (95.47%), Space: 20.1 MB (89.16%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -4,15 +4,12 @@ class Solution:
         if n == 0:
             return [0]
         
-        if n == 1:
-            return [0,1]
         
         res = [0] * (n + 1)
 
         res[0] = 0
-        res[1] = 1
 
-        for i in range(2, n+1):
+        for i in range(1, n+1):
             if i % 2 == 0:
                 res[i] = res[i // 2] 
             else:
