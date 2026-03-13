318c8b981fb8bdf97ab2c4b6e3c23188860c8026
Nikhath Firdose <nikhath.firdose@sjsu.edu>
2026-03-13 11:58:26 -0700

    Time: 3255 ms (5%), Space: 24.3 MB (5.35%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,39 +1,45 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-    
-        ans = set()
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+
+        # Clarifying Questions:
+        # 1. input is expeted to have negatives 0s and positive values right?
+        # 2. will i atleast have min 3 values in my input?
+        # 3. what is the expected length of this array nums? 
+        # 5. can i modify the input??
+
+        # Edge Cases:
+        # 1. expected to return if all values are 0? return any 3 index?
+        # 2. multiple pairs with 0 but all are same values on different index? -> no duplicate triplets
+        # 3. No pair sums up to 0? - return []
+
+
+        # nums = [-1,0,1,3,-2,-1]
+        # output = [(-1,0,1), (3,-2,-1)]
+
+
         nums.sort()
-
-        out = 0
-        in_ = 0
-
         
+        result = set()
 
         for i in range(len(nums)):
 
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
-            
             if nums[i] > 0:
                 break
-            n2 = nums[i]
             seen = set()
-            out += 1
 
             for j in range(i+1, len(nums)):
-                n3 = nums[j]
-                n1 = -n2 -n3
-                in_ += 1
 
-                if n1 in seen:
-                    triplet = tuple(((n1,n2,n3)))
-                    ans.add(triplet)
-
-                seen.add(n3)
-
-        print(out,in_)
-
-        return [list(t) for t in ans]
+                compliment = - (nums[i] + nums[j])
 
 
+                if compliment in seen:
 
+                    triplet = tuple(sorted((nums[i], nums[j], compliment)))
+
+                    result.add(triplet)
+
+
+                seen.add(nums[j])
+
+        
+        return [list(triplet) for triplet in result]
