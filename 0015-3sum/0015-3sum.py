eeddd894dc4b2626b4ad8b440d2143c675a0534b
Nikhath Firdose <nikhath.firdose@sjsu.edu>
2026-03-13 12:25:17 -0700

    Time: 487 ms (89.32%), Space: 22.4 MB (47.63%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,48 +1,47 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
 
-        # Clarifying Questions:
-        # 1. input is expeted to have negatives 0s and positive values right?
-        # 2. will i atleast have min 3 values in my input?
-        # 3. what is the expected length of this array nums? 
-        # 5. can i modify the input??
-
-        # Edge Cases:
-        # 1. expected to return if all values are 0? return any 3 index?
-        # 2. multiple pairs with 0 but all are same values on different index? -> no duplicate triplets
-        # 3. No pair sums up to 0? - return []
-
-
-        # nums = [-1,0,1,3,-2,-1]
-        # output = [(-1,0,1), (3,-2,-1)]
-
-
         nums.sort()
-        
-        result = set()
+        result = []
 
         for i in range(len(nums)):
+
+            seen = set()
 
             if nums[i] > 0:
                 break
 
-            if i > 0 and nums[i] == nums[i - 1]:
+            
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
-            seen = set()
-
-            for j in range(i+1, len(nums)):
-
-                compliment = - (nums[i] + nums[j])
 
 
-                if compliment in seen:
+            left = i + 1
+            right = len(nums) - 1
 
-                    triplet = tuple(sorted((nums[i], nums[j], compliment)))
+            while left < right:
 
-                    result.add(triplet)
+                total = nums[i] + nums[left] + nums[right]
+
+                if total <0:
+                    left += 1
+                elif total > 0:
+                    right -= 1
+
+                else:
+
+                    result.append([nums[i] , nums[left] , nums[right]])
+
+                    left += 1
+                    right -=1
+
+                    while left < right and nums[left] == nums[left-1]:
+                        left += 1
+
+                    while left < right and nums[right] == nums[right+1]:
+                        right -= 1
 
 
-                seen.add(nums[j])
-
-        
-        return [list(triplet) for triplet in result]
+            
+        return result
+                    
