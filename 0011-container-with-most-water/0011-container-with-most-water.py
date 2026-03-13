ff7f44d2885974a4fe7f382eec2bad725c021307
Nikhath Firdose <nikhath.firdose@sjsu.edu>
2026-03-13 11:04:53 -0700

    Time: 63 ms (26.09%), Space: 29.5 MB (95.62%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,25 +1,41 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
 
-        i = 0 
+
+        # Assumptions:
+        # 1. Wont have negative inputs as the height of line needs to always be positive
+        # 2. height array is never empty 
+        
+
+        # Clarifying:
+        # 1. COuld you give me avergae input size ? okay if its 10^5 a linear solution is needed becuase if we have a n^n solution then it will lead to a slow output
+        # 2. can i modify the given inpout?
+        # 3. how high can the indidivusal height[i] be?
+
+
+        # Edge Cases:
+        # 1. all values equal -> len(height) * any value
+        
+        
+        max_so_far = 0
+
+        i = 0
         j = len(height) - 1
-        best = 0
 
         while i < j:
-            chosen_height = min(height[i], height[j])
-            width = j - i
 
-            area =  chosen_height * width
+            area = min(height[i], height[j]) * (j-i)
+            max_so_far = max(max_so_far, area)
 
-            best = max(best, area)
-
-            if height[i] <= height[j]:
+            if height[i] < height[j]:
                 i += 1
-            
             else:
                 j -= 1
 
-        return best
-
-
         
+        return max_so_far
+
+
+
+
+
